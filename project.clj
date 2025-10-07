74e0af22864a384549788d4e32c9700b92dbf400
Michael Sappler <michaelsappler@gmail.com>
2025-10-07 23:27:48 +0530

    update clojure.gdx

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cdq.graphics "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
-                 [com.github.damn/clojure.gdx "531d06d2d7"]
+                 [com.github.damn/clojure.gdx "d6cdfa466c"]
                  [com.github.damn/clojure.gdx.freetype "5a719b264d"]
                  [com.github.damn/clojure.gdx.shape-drawer "238904d145"]
                  ]
